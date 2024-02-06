c1cb1df1d7327e923c8abb1a78dc61dfa15bbca3
mathyasp <mathyastp@gmail.com>
2024-02-06 11:58:44 -0500

    completed the challenges

--- books_app/models.py
+++ books_app/models.py
@@ -25,11 +25,13 @@ class Book(db.Model):
     # The genres, e.g. fiction, sci-fi, fantasy
     genres = db.relationship('Genre', secondary='book_genre', back_populates='books')
 
+    users = db.relationship('User', secondary='favorite_books', back_populates='favorite_books')
+
     def __str__(self):
         return f'<Book: {self.title}>'
 
     def __repr__(self):
-        return f'<Book: {self.title}>'
+        return f'<Book: {self.id}>, <Book: {self.title}>, <Book: {self.publish_date}>'
 
 class Author(db.Model):
     """Author model."""
@@ -58,4 +60,21 @@ class Genre(db.Model):
 book_genre_table = db.Table('book_genre',
     db.Column('book_id', db.Integer, db.ForeignKey('book.id')),
     db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'))
+)
+
+class User(db.Model):
+    """User model."""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), nullable=False, unique=True)
+    favorite_books = db.relationship('Book', secondary='favorite_books', back_populates='users')
+
+    def __str__(self):
+        return f'<User: {self.username}>'
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+favorite_book_table = db.Table('favorite_books',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'))
 )
